1996e3def7f8f35aeaf73355be40c8995c52e57b
masih1999-12 <msh77742@gmail.com>
2024-04-26 06:38:46 +0330

    create views for accounts

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets , mixins
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Team
+from .pagination import DefaultPagination
+from .filters import TeamFilter
+from .serializers import TeamsSerializer
+
+
+class MyTeamsApiVew(
+                    viewsets.GenericViewSet ,
+                    mixins.ListModelMixin ,
+                    mixins.RetrieveModelMixin ,
+                    ):
+    pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = TeamFilter
+    permission_classes = [IsAuthenticated]
+    search_fields = ['name']
+    ordering_fields = ['name']
+    serializer_class = TeamsSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return Team.objects.filter(members__id = user.id).order_by('name')
+    
+class MyTeamsAsLeaderApiVew(
+                    viewsets.GenericViewSet ,
+                    mixins.ListModelMixin ,
+                    mixins.RetrieveModelMixin ,
+                    ):
+    pagination_class = DefaultPagination
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = TeamFilter
+    permission_classes = [IsAuthenticated]
+    search_fields = ['name']
+    ordering_fields = ['name']
+    serializer_class = TeamsSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return Team.objects.filter(leader__id = user.id).order_by('name')
