17dbd781f16f130bea461740d3228d51c9023f7a
masih1999-12 <msh77742@gmail.com>
2024-04-26 06:38:20 +0330

    add and modify serializers for apps

---
+++ reservation/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers , response , status
+from .models import Team ,CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['first_name' ,'last_name']
+
+class TeamsSerializer(serializers.ModelSerializer):
+    members = CustomUserSerializer(many = True)
+    leader = CustomUserSerializer()
+    class Meta:
+        model = Team
+        fields = ['id','name' ,'members' ,'leader']
