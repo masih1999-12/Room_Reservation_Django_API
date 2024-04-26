3ae03aab44740cd449e05dd1b6865574a7c08649
masih1999-12 <msh77742@gmail.com>
2024-04-26 06:36:32 +0330

    modify accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,16 +1,43 @@
 from django.contrib import admin
-from .models import Team , CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm ,AdminPasswordChangeForm
+from .models import CustomUser ,Team
 
-# Register your models here.
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = CustomUser
 
-@admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'phone')
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
 
+class CustomUserAdmin(BaseUserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name', 'email', 'phone', 'profile_image')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username', 'email')
+
+    # Add change password functionality
+    change_password_form = AdminPasswordChangeForm
+    change_password_template = None
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    list_display = ('name', 'get_members', 'leader')
-    def get_members(self, obj):
-        return obj.members()
-        
+    pass
