58fe78f14119d0e09d3516d7a6df4a5330c618b8
masih1999-12 <msh77742@gmail.com>
2024-04-28 14:27:13 +0330

    modify reservation/filters.py for new view

--- reservation/filters.py
+++ reservation/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet
 
-from .models import Room ,Reservation
+from .models import Room ,Reservation ,Comment
 
 class RoomFilter(FilterSet):
     class Meta:
@@ -16,7 +16,13 @@ class ReservationFilter(FilterSet):
         fields = {
             'room__name': ['contains'],
             'team__name': ['contains'],
-            'date':['exact'],
-            'date':['gte'],
-            'date':['lte'],
+            'date':['exact' , 'gte' ,'lte'],
+        }
+        
+class CommentFilter(FilterSet):
+    class Meta:
+        model = Comment
+        fields = {
+            'reservation__room__name': ['contains'],
+            'reservation__date':['exact' ,'gte' ,'lte'],
         }
