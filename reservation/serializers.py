41991340679e6f0a8f5ebfa28d91edfb85f6d6d4
masih1999-12 <msh77742@gmail.com>
2024-04-28 14:28:00 +0330

    add serializer for comment

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers , response , status
-from .models import Room ,Reservation
+from django.utils import timezone
+from .models import Room ,Reservation ,Comment
 
 from accounts.models import  CustomUser, Team
 
@@ -26,3 +27,32 @@ class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = ['id','room','team','date','start','end']
+        
+class CommentSerializer(serializers.ModelSerializer):
+    reservation = ReservationSerializer()
+    class Meta:
+        model = Comment
+        fields = ['id','text','point','reservation']
+    def get_fields(self):
+        fields = super().get_fields()
+        if self.context.get('request') and (self.context['request'].method == 'POST'):
+            fields['reservation'] = serializers.PrimaryKeyRelatedField(Reservation)
+        return fields
+    def create(self, validated_data):
+        user = self.context['user']
+        reservation = validated_data['reservation']
+        
+        if reservation.team.leader != user:
+            raise serializers.ValidationError('You not team leader for this reservation!')
+        
+        date = timezone.now().date()
+        time = timezone.now().time()
+        
+        if reservation.date <= date and reservation.end <= time:
+            raise serializers.ValidationError('Reservation not finished!')
+        
+        if validated_data['point'] > 5 or validated_data['reservation'] < 0:
+            raise serializers.ValidationError('Unvalid point!')
+        
+        return validated_data
+        
